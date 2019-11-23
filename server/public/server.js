93cea2260b85e8c9157e6fae53f4e55a8ab1d607
Josh Ganote <j.ganote0825@gmail.com>
2019-11-22 22:42:57 -0600

    added back the client folder I had deleted out of panic when I changed my server folders around. Not sure if that helps

--- server/public/server.js
+++ server/public/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-app.use(express.static('server'));
+app.use(express.static('server/public'));
 
 const calculator = {
     calculation = 'Incoming Calculation',
