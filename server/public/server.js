98b32292af055833feb02925af505bd8ad036ae2
Josh Ganote <j.ganote0825@gmail.com>
2019-11-22 20:58:24 -0600

    chaning folder structure around to match lesson video

---
+++ server/public/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.use(express.static('server/public'));
+
+
+app.get('/api/calculator', (req,res) => {
+    console.log('im working!!!')
+    // const solveObject = req.body.solveObject;
+
+    // const val1 = parseInt(solveObject.input1);
+    // const val2 = parseInt(solveObject.input2);
+    // const mathOp = 
+    res.send(418);
+});
+
+app.listen(PORT, () => {
+    console.log('listening on port:', PORT);
+});
+
+
+
+
+
+
+// const inputVal = {
+//     firstInput: 
+//     mathOperator:
+//     secondInput:
+//     total: 
+// };
