d322db0ca711f6d78f55ba48d63f8bbe16043d7e
Josh Ganote <j.ganote0825@gmail.com>
2019-11-23 16:06:42 -0600

    I'm pretty sure I got my GET and POST to work in Postman properly. Now need to figure out mathOperations in GET

--- server/server.js
+++ server/server.js
@@ -9,11 +9,14 @@ app.use(bodyParser.json());
 
 app.use(express.static('server/public'));
 
+// Not totally sure what I'm using this for Im just trying to adapt this from Friday's video
 const calculator = {
+    // not sure why I had '=' here instead of ':' I knew better.. ha
     calculation: 'Incoming Calculation',
-    newCalcNumber: 0,
+    runningCalcTotal: 0,
 }
 
+// this needs to GET info from client input 
 app.get('/api/calculator', (req,res) => {
     console.log('im working!!!')
     // const solveObject = req.body.solveObject;
@@ -21,7 +24,23 @@ app.get('/api/calculator', (req,res) => {
     // const val1 = parseInt(solveObject.input1);
     // const val2 = parseInt(solveObject.input2);
     // const mathOp = 
-    res.send({newCalcNumber: calculator.newCalcNumber });
+
+    // couldn't take a number, has to be in an object like this
+    
+    res.send({runningCalcTotal: calculator.runningCalcTotal});
+});
+
+// this needs to capture calculator input from client side when '=' is pressed and POST to server
+app.post('/api/equalsign', (req,res) => {
+    const equalBtn = parseFloat(req.body.equalBtn).toFixed(2);
+    console.log(equalBtn);
+    
+    if (equalBtn) {
+        calculator.runningCalcTotal += equalBtn;
+        res.send(201);
+    } else {
+        res.send(500);
+    }
 });
 
 app.listen(PORT, () => {
